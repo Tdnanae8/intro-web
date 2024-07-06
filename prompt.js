24986cdfd4f5d00fb1aa5db5e60fc518c9739275
T'iiare B'aezaa <tiare.baeza@soymas.cl>
2024-07-05 22:44:51 -0400

    estructura de control

---
+++ prompt.js
@@ -0,0 +1,8 @@
+alert("Bienvenido al número secreto")
+let userChoice = prompt("Elige un número entre 1 y 10")
+alert("Tú elección fue: " + userChoice)
+let secretNumber = 2
+
+if (userChoice == secretNumber){
+    alert("Ganaste")
+}
