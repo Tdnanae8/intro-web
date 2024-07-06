26bb966540cde094a698982c9ee0cff817f0af49
T'iiare B'aezaa <tiare.baeza@soymas.cl>
2024-07-05 23:10:46 -0400

    numeros al azar

--- prompt.js
+++ prompt.js
@@ -1,8 +1,10 @@
 alert("Bienvenido al número secreto")
 let userChoice = prompt("Elige un número entre 1 y 10")
 alert("Tú elección fue: " + userChoice)
-let secretNumber = 2
+let secretNumber = Math.floor (Math.random() * 10)
 
 if (userChoice == secretNumber){
-    alert("Ganaste")
-}
+    alert("¡Ganaste!")
+} else { 
+    alert("¡Perdiste! el número secreto era : " + secretNumber)
+} 
